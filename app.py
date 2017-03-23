3d5d062182eb0f85ba7df428140bdfd559fb98fd
Rabiul Islam <rb60041@gmail.com>
2017-03-23 23:57:07 +0600

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,7 @@ def jsonreturn(var):
         name = " ".join(elems[1].text.split())
         father =  " ".join(elems[2].text.split())
         madrasa =  " ".join(elems[3].text.split())
+        markaj =  " ".join(elems[4].text.split())
         
         t = []
         n = 2
@@ -38,14 +39,14 @@ def jsonreturn(var):
             t.append(numbers[n].text.strip())
             n+=3
         kitab = "বুখারি ১মঃ {}\nমুসলিম ১মঃ {} \nতিরমিযী ১মঃ {} \nআবু দাউদঃ {} \nবুখারী ২য়ঃ {} \nমুসলিম ২য়ঃ {} \nতিরমিযী ২য়ঃ {} \nনাসাঈ ও ইবনু মাজাহঃ {} \nত্বহাবীঃ {} \nমুআত্তানঃ {}".format(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9])
-        msg = (name,father,madrasa,'মোট নাম্বার: {}'.format(totalnumber),'বিভাগ: {}'.format(division),'মেধাস্থান: {}'.format(medha))
+        msg = (name,father,madrasa,markaj'মোট নাম্বার: {}'.format(totalnumber),'বিভাগ: {}'.format(division),'মেধাস্থান: {}'.format(medha))
+        return jsonify({ "messages": [ {"text": "{}\n{}".format(msg[0], msg[1])},
+                              {"text": "{}\n{}".format(msg[2], msg[3])},
+                              {"text": "{}".format(kitab)},
+                              {"text": "{}".format(msg[4])},
+                              {"text": "{}".format(msg[5], msg[6])} ] 
+                       })
+    
     except:
-        msg = "কোন সমস্যা হইসে মনে হয় :/".split()
-    return jsonify({ "messages": [ {"text": "{}".format(msg[0])},
-                                  {"text": "{}".format(msg[1])},
-                                  {"text": "{}".format(msg[2])},
-                                  {"text": "{}".format(kitab)},
-                                  {"text": "{}".format(msg[3])},
-                                  {"text": "{}".format(msg[4])},
-                                  {"text": "{}".format(msg[5])} ] 
-                   })
+        return jsonify({ "messages": [ {"text": "কোন সমস্যা হয়েছে মনে হয় :( \nআবার চেষ্টা করুন !"} ]}
+
