823f759f02b9a915a27e0b6abce0fe9e42ad47bf
Rabiul Islam <rb60041@gmail.com>
2017-03-23 21:08:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -14,4 +14,24 @@ def hello_world():
 
 @app.route('/<var>')
 def jsonreturn(var):
+    payload = {'years':'2016',
+           'ClassName':1,
+           'Roll':var}
+    try:
+        r = requests.post(url,payload,headers=headers)
+        r.encoding = 'utf-8'
+        html = bs(r.content,'html.parser')
+        elems = html.select('div table tbody tr th[colspan]')
+        numbers = html.select('div table tbody tr td[class]')
+
+        totalnumber = numbers[32].text.split()[0]
+        division = numbers[33].text.split()[0]
+        medha = numbers[36].text.split()[0]
+
+        name = " ".join(elems[1].text.split())
+        father =  " ".join(elems[2].text.split())
+        madrasa =  " ".join(elems[3].text.split())
+        msg = (name,father,madrasa,'মোট নাম্বার: {}'.format(totalnumber),'বিভাগ: {}'.format(division),'মেধাস্থান: {}'.format(medha))
+    except:
+        msg = "কোন সমস্যা হইসে মনে হয় :/ "
     return jsonify({ "messages": [ {"text": "Welcome to our store!"}, {"text": "How can I help you?"} ] })
