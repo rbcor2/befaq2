24914691152cc7bb4d4229ea168a8e13c0411aa7
Rabiul Islam <rb60041@gmail.com>
2017-03-23 23:41:27 +0600

    Update app.py

--- app.py
+++ app.py
@@ -24,19 +24,27 @@ def jsonreturn(var):
         elems = html.select('div table tbody tr th[colspan]')
         numbers = html.select('div table tbody tr td[class]')
 
-        totalnumber = numbers[32].text.split()[0]
-        division = numbers[33].text.split()[0]
-        medha = numbers[36].text.split()[0]
+        totalnumber = numbers[32].text.strip()
+        division = numbers[33].text.strip()
+        medha = numbers[36].text.strip()
 
         name = " ".join(elems[1].text.split())
         father =  " ".join(elems[2].text.split())
         madrasa =  " ".join(elems[3].text.split())
+        
+        t = []
+        n = 2
+        for i in range(10):
+            t.append(numbers[n].text.strip())
+            n+=3
+        kitab = "বুখারি ১মঃ {}\nমুসলিম ১মঃ {} \nতিরমিযী ১মঃ {} \nআবু দাউদঃ {} \nবুখারী ২য়ঃ {} \nমুসলিম ২য়ঃ {} \nতিরমিযী ২য়ঃ {} \nনাসাঈ ও ইবনু মাজাহঃ {} \nত্বহাবীঃ {} \nমুআত্তানঃ {}".format(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9])
         msg = (name,father,madrasa,'মোট নাম্বার: {}'.format(totalnumber),'বিভাগ: {}'.format(division),'মেধাস্থান: {}'.format(medha))
     except:
         msg = "কোন সমস্যা হইসে মনে হয় :/".split()
     return jsonify({ "messages": [ {"text": "{}".format(msg[0])},
                                   {"text": "{}".format(msg[1])},
                                   {"text": "{}".format(msg[2])},
+                                  {"text": "{}".format(kitab)},
                                   {"text": "{}".format(msg[3])},
                                   {"text": "{}".format(msg[4])},
                                   {"text": "{}".format(msg[5])} ] 
