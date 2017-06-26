21fe67c4de23c015f9aeb279ca4314dace2a3fc3
rbcor2 <rbcor2@users.noreply.github.com>
2017-06-26 23:09:05 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = '873yiuwehrily-needs-to-be-changedopsskdjfjfk92837489634'
-SQLALCHEMY_DATABASE_URI = "sqlite:///befaq.db"
+SQLALCHEMY_DATABASE_URI = "postgres://mlxodpnnjrwuok:2b84fe1535ef25d6c561a4982b0128ebe6828c3280d8bf48ab214838f25c9958@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/dsb4kb4rsfepb"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-DEBUG = True
+DEBUG = False
