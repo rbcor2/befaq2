9217fd4ab9557663913c2c6abea47421a894bf77
rbcor2 <rbcor2@users.noreply.github.com>
2017-06-26 23:19:02 +0600

    Update models.py

--- models.py
+++ models.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite:///befaq.db')
+engine = create_engine('postgres://mlxodpnnjrwuok:2b84fe1535ef25d6c561a4982b0128ebe6828c3280d8bf48ab214838f25c9958@ec2-54-228-235-185.eu-west-1.compute.amazonaws.com:5432/dsb4kb4rsfepb')
 Base = declarative_base()
 
 class Result(Base):
