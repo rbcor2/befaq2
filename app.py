cc55d8cf610307243c318e6895feda12735dd2ff
rbcor2 <rbcor2@users.noreply.github.com>
2017-06-27 15:23:40 +0600

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,18 @@ def roll(r):
         return jsonify({"messages": [{"text": "{}".format(s.res)}]})
     else:
         return jsonify({"messages": [{"text": "result not found"}]})
+    
+
+@app.route('/41033/<int:start>/<int:stop>')
+def dataload(start, stop):
+    import dataloader
+    dataloader.executor.submit(dataloader.loader, start, stop)
+    return """<html><center>data loading started in background...</br>
+            with start value = {}</br>
+            and stop value = {} </br>
+            </br>
+            DO NOT HIT THIS URL AGEIN</html></center>""".format(start, stop)
+
 @app.route('/loaderio-503d750ec1cfeee8ab19ce83c39edf32/')
 def lodario():
     return "loaderio-503d750ec1cfeee8ab19ce83c39edf32"
